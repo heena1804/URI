f568ca91e23b70d9cf5a5014395cce40b054fef6
Heena Jain <heena.jain98@gmail.com>
2016-02-04 19:27:16 +0530

    1555.cpp

---
+++ 1555.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int x,y;
+		cin>>x>>y;
+		int r=9*x*x + y*y;
+		int b=2*x*x + 25*y*y;
+		int c=-100*x+y*y*y;
+		if(r>b&&r>c) cout<<"Rafael ganhou"<<endl;
+		else if(b>r&&b>c) cout<<"Beto ganhou"<<endl;
+		else cout<<"Carlos ganhou"<<endl;
+	}
+	return 0;
+}
